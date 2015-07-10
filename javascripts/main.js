c464537a8328058e5d39418cd1806ed19ca42d4e
nischalkarki <nischal@redbeastie.com>
2015-07-10 14:08:00 +0545

    content changed

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,14 @@
-console.log('This would be the main JS file.');
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+if (hourNow>18){
+	greeting="Good evening"
+}else if(hourNow>12){
+	greeting="Good afternoon"
+}else if(hourNow>0){
+	greeting="Good morning"
+}else{
+	greeting="welcome"
+}
+
+document.write('<h3>'+greeting+'</h3>');
